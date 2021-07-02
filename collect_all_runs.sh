15f8827bcea411ead061f86211b4695fe59210be
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2021-07-02 10:21:21 +0100

    joblib runs

--- collect_all_runs.sh
+++ collect_all_runs.sh
@@ -9,7 +9,7 @@ python collect_runs.py --gating=None --cuda=False  --ctx_scaling=8 --lrate_sgd=0
 python collect_runs.py --gating=None --cuda=False  --ctx_scaling=8 --lrate_sgd=0.1 --centering=False --weight_init=1e-3 --training_schedule='interleaved' --ctx_avg=False --save_dir='baseline_interleaved' --verbose=False;
 
 # blocked, gated 
-python collect_runs.py --gating=manual --cuda=True  --ctx_scaling=1 --lrate_sgd=0.01 --centering=False --weight_init=1e-2 --save_dir='gated_blocked' --verbose=False;
+python collect_runs.py --gating=manual --cuda=False  --ctx_scaling=1 --lrate_sgd=0.01 --centering=False --weight_init=1e-2 --save_dir='gated_blocked' --verbose=False;
 
 # sla, gated
 python collect_runs.py --gating=SLA --cuda=False  --ctx_scaling=2 --lrate_sgd=0.03 --lrate_hebb=0.03 --centering=True --weight_init=1e-3 --save_dir='sla_blocked' --verbose=False;
--- collect_runs.py
+++ collect_runs.py
@@ -12,45 +12,47 @@ from logger import MetricLogger
 from model import Gatednet, Nnet
 from trainer import Optimiser, train_model
 from parameters import parser
-
+from joblib import Parallel, delayed
 
 args = parser.parse_args()
 # overwrite cuda argument depending on GPU availability
 args.cuda = args.cuda and torch.cuda.is_available()
 
+def train_model(i_run):
+    print('run {} / {}'.format(str(i_run),str(args.n_runs)))
+        
+    # create checkpoint dir 
+    run_name = 'run_'+str(i_run)
+    save_dir = Path("checkpoints") / args.save_dir / run_name
+    
+
+    # get (cuda) device
+    args.device,_ = get_device(args.cuda)
+    
+    # get dataset
+    dataset = make_dataset(args)
+    
+    # instantiate logger, model and optimiser
+    logger = MetricLogger(save_dir)
+    if args.gating=='manual':
+        model = Gatednet(args)
+    else:
+        model = Nnet(args)
+    optim = Optimiser(args)
+
+    # send model to GPU
+    model = model.to(args.device)
+
+    # train model
+    train_model(args, model,optim,dataset, logger)
+    
+
+    # save results 
+    if args.save_results:
+        save_dir.mkdir(parents=True)
+        logger.save(model)
 
 if __name__ == "__main__":
     
-    for i_run in range(args.n_runs):
-        print('run {} / {}'.format(str(i_run),str(args.n_runs)))
-        
-        # create checkpoint dir 
-        run_name = 'run_'+str(i_run)
-        save_dir = Path("checkpoints") / args.save_dir / run_name
-        
-
-        # get (cuda) device
-        args.device,_ = get_device(args.cuda)
-        
-        # get dataset
-        dataset = make_dataset(args)
-        
-        # instantiate logger, model and optimiser
-        logger = MetricLogger(save_dir)
-        if args.gating=='manual':
-            model = Gatednet(args)
-        else:
-            model = Nnet(args)
-        optim = Optimiser(args)
-
-        # send model to GPU
-        model = model.to(args.device)
-
-        # train model
-        train_model(args, model,optim,dataset, logger)
-        
-
-        # save results 
-        if args.save_results:
-            save_dir.mkdir(parents=True)
-            logger.save(model)
+    Parallel(n_jobs=30,verbose=10)(delayed(train_model)(i_run) for i_run in range(args.n_runs))
+      
